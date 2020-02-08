3ee1c2fb64dfa2fb8b373f9a5f6d89ab9e70456d
jeyganesh7 <54430564+jeyganesh7@users.noreply.github.com>
2020-02-08 20:36:00 +1100

    Error message for invalid cmmd

--- robot/src/robot/User_Input.java
+++ robot/src/robot/User_Input.java
@@ -5,21 +5,69 @@ import java.util.Scanner;
 public class User_Input 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		
+		// TODO Auto-generated method stub
+		//Scanner input = new Scanner(System.in);	
 		String s; 
 		Scanner sc = new Scanner(System.in);
  		System.out.println("Enter ROBOT Commands - seperated by comma: "); 
  		s = sc.nextLine();  
  		
- 		String[] question;
+ 		//if (!s.contains("f") && !s.contains("b") && !s.contains("l") && !s.contains("r") )
+ 		
+        String[] question;
          
         /* delimiter */
         String delimiter = ",";
+        
         question = s.split(delimiter);
-        calculation.robot_movement(question);     
+        check_errors(question);
 	}
+	
+	public static void check_errors (String[] question)
+	{
+        int error = 0;
+        
+        for (int i = 0; i < question.length; i++)
+        {
+        	String b = String.valueOf(question[i].charAt(0));
+        	//if(b !="f" || b !="b" || b !="l" || b !="r")
+        	if (!b.contains("f") && !b.contains("b") && !b.contains("l") && !b.contains("r") )
+        	{
+        		//System.out.println("checkpoint 1");
+        		error++; 		
+        	}
+        	else
+        	{
+        		error = error;
+        	}
+        }
+        if(error > 0)
+        {
+        	System.out.println('\n'+"Invalid command found!"+'\n');
+        	System.out.println("Do you want to try again? Enter Y or N"+'\n');
+        	//char e;
+        	Scanner dc = new Scanner(System.in);
+        	char e = dc.next().charAt(0);
+        	
+        	if(e =='Y')
+        	{
+        		//System.out.println("reached here");
+        		main(null);
+        	}
+        	else 
+        	{
+        		System.out.println("Bye bye...see you later");
+        		System.exit(0);
+        	}
+        }
+        else
+        {	
+        calculation.robot_movement(question);
+        }
+ 		
+	} 	
 
 }
